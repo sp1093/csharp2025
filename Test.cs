7657fcf6da54b4a216b7fcf1b1386478ac168203
sp1093 <sp.develop2@gmail.com>
2025-12-21 00:15:30 +0530

    Second commit

---
+++ Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025
+{
+    internal class Test
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is a test file.");
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
