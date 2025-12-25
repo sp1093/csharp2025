aacb3a72a3fbd080dd20b9b07581c0ea8d8118a4
sp1093 <sp.develop2@gmail.com>
2025-12-25 19:22:47 +0530

    new Test Program

---
+++ TestProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025
+{
+    internal class TestProgram
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("TestProgram output");
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
--- obj/Debug/net8.0/2025.AssemblyInfo.cs
+++ obj/Debug/net8.0/2025.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52a128a1a1f1b1ccf9ce8b8773ee804f745efe77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca5462e62769a1eb201f6f1160c7e2ba0d5b3c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
