86e6c3b913208bc1aed5bf6256655f881268c70b
sp1093 <sp.develop2@gmail.com>
2025-12-27 12:20:44 +0530

    Amend second commit

--- TestProgram.cs
+++ TestProgram.cs
@@ -13,33 +13,7 @@ namespace _2025
         {
             Console.WriteLine("TestProgram output");
 
-public static void Main(string[] args)
-        {
-            Console.WriteLine("");
 
-
-            /// <summary> 
-            /// For more information about writing UDOs, please refer to http://go.microsoft.com/fwlink/?LinkID=623598&clcid=0x409 
-            /// </summary>
-            [SqlUserDefinedCombiner]
-            public class MyCombiner : ICombiner
-        {
-            public override IEnumerable<IRow> Combine(IRowset left, IRowset right, IUpdatableRow output)
-            {
-                int i = 0;
-                if (right.Rows.GetEnumerator().MoveNext())
-                {
-                    foreach (IRow row in left.Rows)
-                    {
-                        output.Set(i++, row);
-                        yield return output.AsReadOnly();
-                    }
-                }
-            }
-        }
-
-        Console.ReadLine();
-        }
                 
 
         Console.ReadLine();
