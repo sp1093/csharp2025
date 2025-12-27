2709fe73caa0c87ca837f9bf0c07c6db9af15e30
sp1093 <sp.develop2@gmail.com>
2025-12-27 12:20:44 +0530

    again file submitted

--- TestProgram.cs
+++ TestProgram.cs
@@ -17,28 +17,8 @@ public static void Main(string[] args)
         {
             Console.WriteLine("");
 
-
-            /// <summary> 
-            /// For more information about writing UDOs, please refer to http://go.microsoft.com/fwlink/?LinkID=623598&clcid=0x409 
-            /// </summary>
-            [SqlUserDefinedCombiner]
-            public class MyCombiner : ICombiner
-        {
-            public override IEnumerable<IRow> Combine(IRowset left, IRowset right, IUpdatableRow output)
-            {
-                int i = 0;
-                if (right.Rows.GetEnumerator().MoveNext())
-                {
-                    foreach (IRow row in left.Rows)
-                    {
-                        output.Set(i++, row);
-                        yield return output.AsReadOnly();
-                    }
-                }
-            }
-        }
-
-        Console.ReadLine();
+          Console.WriteLine("Teting for conflicts");
+            Console.ReadLine();
         }
                 
 
